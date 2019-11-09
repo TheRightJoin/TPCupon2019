281bcbaa96640249395e0ec381dc09e38de171a9
Lucas <lucascheistwer@gmail.com>
2019-11-09 14:29:33 -0300

    procedure modificar cliente

---
+++ modificarCliente.sql
@@ -0,0 +1,14 @@
+create procedure THE_RIGHT_JOIN.modificarCliente
+@dni numeric(18,0) = null,
+@nombre nvarchar(255) = NULL,
+@apellido nvarchar(255) = NULL,
+@mail nvarchar(255) = NULL,
+@direccion nvarchar(255) = NULL,
+@ciudad nvarchar(255) = NULL,
+@fechaNac datetime = NULL,
+@telefono numeric(18,0) = NULL
+AS
+UPDATE THE_RIGHT_JOIN.Cliente set 
+Cli_Nombre=@nombre,Cli_Apellido=@apellido,Cli_Mail=@mail,Cli_Direccion=@direccion,
+Cli_Ciudad=@ciudad,Cli_Fecha_Nac=@fechaNac,Cli_Telefono=@telefono
+WHERE Cli_Dni = @dni
