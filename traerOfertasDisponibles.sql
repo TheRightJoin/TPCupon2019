824b91d15039c5370c725d4df146821be26b170f
Lucas <lucascheistwer@gmail.com>
2019-11-13 14:59:03 -0300

    bug fix traer ofertas sql

--- traerOfertasDisponibles.sql
+++ traerOfertasDisponibles.sql
@@ -1,4 +1,5 @@
 create function THE_RIGHT_JOIN.traerOfertasDisponibles(@fecha date)
 returns table
 as
-return select * from THE_RIGHT_JOIN.Oferta  where Oferta_Disponible = 1 and @fecha < Oferta_Fecha_Ven
+return select * from THE_RIGHT_JOIN.Oferta  where Oferta_Disponible = 1 and @fecha =< Oferta_Fecha_Ven
+and @fecha >= Oferta_Fecha
