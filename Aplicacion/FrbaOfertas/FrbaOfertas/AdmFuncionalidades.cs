6a94e78d265e23d85ea7f9ccd7dda62d5b04e496
unknown <ivounter@gmail.com>
2019-11-22 16:57:22 -0300

    Trae funcionalidades segun rol, Agregado roles y func a migracion y admFunc

---
+++ Aplicacion/FrbaOfertas/FrbaOfertas/AdmFuncionalidades.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrbaOfertas
+{
+	class AdmFuncionalidades
+	{
+		public static DataSet obtenerFuncxRol(int idRol)
+		{
+			string connString = ConfigurationManager.ConnectionStrings["THE_RIGHT_JOIN"].ConnectionString;
+			SqlConnection conn = new SqlConnection(connString);
+			String query = "SELECT * FROM THE_RIGHT_JOIN.traerFuncDelRol(@rol)";
+			SqlCommand cmd = new SqlCommand(query, conn);
+			cmd.Parameters.Add("@rol", SqlDbType.Int).Value = idRol;
+			return ConectorBDD.cargarDataSet(conn, cmd);
+		}
+
+	}
+}
